cf7d20edb3efc7805ca9cad48e41284db0b59756
Andras Karasz <karaszandris@gmail.com>
2021-06-11 14:30:19 +0200

    feat: in-file grep

--- grep.py
+++ grep.py
@@ -2,6 +2,19 @@ import sublime
 import sublime_plugin
 
 
-class GrepCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+class GrepCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		self.window.show_input_panel("Grep:", "", self.on_done, None, None)
+
+	def on_done(self, text):
+		self.window.active_view().run_command("keep_lines", { "containing": text })
+
+class KeepLinesCommand(sublime_plugin.TextCommand):
+	def run(self, edit, containing):
+		rr = self.view.find_all(containing)
+		keep = [self.view.rowcol(self.view.full_line(r).begin())[0] for r in self.view.find_all(containing, sublime.LITERAL)]
+		total = list(range(self.view.rowcol(self.view.size())[0] + 1))
+		delete = [x for x in total if x not in keep]
+
+		for y in reversed(delete):
+			self.view.erase(edit, self.view.full_line(self.view.text_point(y, 0)))
